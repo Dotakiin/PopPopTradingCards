8934f46e51a9557790623b0fbe5155d437e2bc00
Dotakiin <47369531+Dotakiin@users.noreply.github.com>
2020-03-17 12:04:51 -0500

    skeleton

---
+++ PopPopTradingCards/PopPopTradingCards/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PopPopTradingCards.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
