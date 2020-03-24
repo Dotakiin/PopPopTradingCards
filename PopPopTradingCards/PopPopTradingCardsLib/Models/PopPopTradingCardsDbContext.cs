64a04226cb1438d2767a9a8f8f94c145a309e08d
Trostka <kyletrost@verizon.net>
2020-03-24 16:32:48 -0500

    set up code first

---
+++ PopPopTradingCards/PopPopTradingCardsLib/Models/PopPopTradingCardsDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace PopPopTradingCardsLib.Models
+{
+    public class PopPopTradingCardsDbContext : DbContext
+    {
+        public PopPopTradingCardsDbContext()
+        {
+
+        }
+
+        public PopPopTradingCardsDbContext(DbContextOptions<PopPopTradingCardsDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<MagicCard> MagicCards { get; set; }
+        public virtual DbSet<BaseballCard> BaseballCards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasMany(e => e.MagicCards);
+
+                entity.HasMany(e => e.BaseballCards);
+            });
+
+            modelBuilder.Entity<MagicCard>(entity =>
+            {
+                entity.HasOne(e => e.User)
+                    .WithMany(e => e.MagicCards);
+            });
+
+            modelBuilder.Entity<BaseballCard>(entity =>
+            {
+                entity.HasOne(e => e.User)
+                    .WithMany(e => e.BaseballCards);
+            });
+        }
+    }
+}
--- PopPopTradingCards/PopPopTradingCardsLib/Models/User.cs
+++ PopPopTradingCards/PopPopTradingCardsLib/Models/User.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+[Table("User", Schema = "TradingCardTracker")]
 public class User
 {
 	public User()
 	{
 	}
 
+	[Key]
 	public int Id { get; set; }
 
+	[Required]
 	public string Username { get; set; }
 
+	[Required, DataType(DataType.Password)]
 	public string Password { get; set; }
+	public virtual ICollection<MagicCard> MagicCards { get; set; }
+	public virtual ICollection<BaseballCard> BaseballCards { get; set; }
 }
