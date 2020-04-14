1776d8feafa5b455fdac5bf3dee0bad1ab3f76a5
Trostka <kyletrost@verizon.net>
2020-04-14 09:25:37 -0500

    added view and view model for other collections

---
+++ PopPopTradingCards/PopPopTradingCardsWebUi/Models/OtherCollectionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PopPopTradingCardsLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PopPopTradingCardsWebUI.Models
+{
+    public class OtherCollectionViewModel
+    {
+        public string Username { get; set; }
+        public IEnumerable<MagicCard> MagicCards { get; set; }
+        public IEnumerable<BaseballCard> BaseballCards { get; set; }
+    }
+}
